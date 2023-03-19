e410b0ee70fe90655197edda5f52de6d59867f3d
User <sawentino@gmail.com>
2023-03-19 14:46:13 +0200

    complete task 1-3

--- task1.py
+++ task1.py
@@ -1,2 +1,11 @@
-a = "I love Python \n"
-print(a * 42)   #надрукувати фразу “I love Python” 42 рази
+# повідомляти — чи є введений текст “числом” чи “словом”
+message = str(input("Please print message :"))
+if message == "":
+    print("Немає значення =(")
+elif message.isdigit():  # якщо число
+    if int(message) % 2 == 0:
+        print(message, "це парне число")
+    else:
+        print(message, "не парене число")
+else:
+    print("Довжина слова", len(message))  # виводить довжину строки
